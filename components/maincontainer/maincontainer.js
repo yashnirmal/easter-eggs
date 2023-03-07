8a65030b57f02e84f70881a5f5028231a5162af1
yash nirmal <yash15nirmal@gmail.com>
2023-03-07 23:02:14 +0530

    minor changes

--- components/maincontainer/maincontainer.js
+++ components/maincontainer/maincontainer.js
@@ -1,7 +1,6 @@
 import DrawingBoard from "../drawingboard/drawingboard";
 import NeverGonnaGiveYouUp from "../easter-eggs/1-never-gonna-give-you-up/never-gonna-give-you-up"
 import Demo from "../easter-eggs/0-demo/your-easter-name";
-import WillSmithSlap from "../easter-eggs/2-willsmith-slap/willsmith-slap"
 
 export default function MainContainer(){
 	return (
@@ -9,8 +8,9 @@ export default function MainContainer(){
 			<div className="w-[90%] grid md:grid-cols-3 sm:grid-cols-2 grid cols-1 row-span-2 gap-6 text-xl">
 				<DrawingBoard />
 				<NeverGonnaGiveYouUp />
-				<WillSmithSlap />
 				<Demo />
+				<Demo />
+				
 			</div>
 		</div>
 	)
