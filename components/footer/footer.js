4c0fb2f93f22fcbe89784e5d261a300124e4cae7
yash nirmal <yash15nirmal@gmail.com>
2023-03-08 17:42:13 +0530

    added footer

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -2,7 +2,6 @@ export default function Footer(){
 	return (
 		<div className="flex items-center justify-center p-12">
 			<span>OpenSource | <a className="underline" href="https://github.com/yashnirmal/easter-eggs" target="blank">Github</a></span>
-			
 		</div>
 	)
 }
