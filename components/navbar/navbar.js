dcad7ee9567be0737346e38f4df58edd6f14f346
yash nirmal <yash15nirmal@gmail.com>
2023-03-08 17:53:37 +0530

    added wobble effect

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,7 +1,7 @@
 export default function Navbar(){
 	return (
 		<div className="md:text-6xl sm:text-5xl text-4xl font-bold flex justify-center items-center p-10">
-			<h1 className="text-center">Easter Eggs<span className="animate-egg-wobble">🥚</span></h1>
+			<h1 className="text-center relative">Easter Eggs<span className="absolute cursor-default animate-egg-wobble">🥚</span></h1>
 		</div>
 	)
 }
